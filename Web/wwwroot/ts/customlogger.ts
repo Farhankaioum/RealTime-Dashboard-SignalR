1cfb634a146545e8a2eccc1c71a25bed1620aeb7
Farhankaioum <km1472233@gmail.com>
2021-04-08 23:07:23 +0600

    custom typescript logger added using SignalR package

---
+++ Web/wwwroot/ts/customlogger.ts
@@ -0,0 +1,9 @@
+﻿import { ILogger, LogLevel } from "@microsoft/signalr";
+
+export class CustomLogger implements ILogger {
+    log(logLevel: LogLevel, message: string) {
+        // use this method for write logging in anywhere
+
+        console.log(`${logLevel} :: ${message}`);
+    }
+}
